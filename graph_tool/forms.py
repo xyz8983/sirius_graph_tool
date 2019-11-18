d5824f2a914c6600d0f486e73960bdebcb749222
Zheng, Yuhang <YZheng83@MassMutual.com>
2019-11-18 14:52:20 -0500

    support multiple files uploading

--- graph_tool/forms.py
+++ graph_tool/forms.py
@@ -1,13 +1,32 @@
 from django import forms
+from .models import UploadFileField
 
 class UploadJSONandImage(forms.Form):
     file_field = forms.FileField(
-        # widget=forms.ClearableFileInput(attrs={'multiple': True})
-        error_messages={'required': 'Please upload json file and image files'}
+        widget=forms.ClearableFileInput(attrs={'multiple': True})
     )
 
+    @classmethod
+    def validate_extension(cls, files_list):
+        """files_list: a list of uploadedFile obj"""
+        all_passed = True
+        for f in files_list:
+            extension = cls.get_extension(f.name)
+            all_passed = all_passed and (extension in ['json', 'png'])
+        print('all_passed', all_passed)
+        return all_passed
 
     @staticmethod
     def get_extension(file_name):
-        # extensions = set(['json', 'jpg', 'png']),
-        pass
+        if file_name is None:
+            return ''
+        elif len(file_name) == 0:
+            return ''
+        else:
+            return file_name[file_name.rfind('.') + 1:].lower()
+
+
+class UploadJSONImageForm(forms.ModelForm):
+    class Meta:
+        model = UploadFileField
+        fields = ('file_field',)
--- graph_tool/views.py
+++ graph_tool/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import TemplateView
-from .forms import UploadJSONandImage
+from .forms import UploadJSONandImage, UploadJSONImageForm
+from .models import UploadFileField
+import shutil
+import os
+
 
 
 def index(request):
@@ -24,19 +28,40 @@ class HomePageView(TemplateView):
         return context
 
     def post(self, *args, **kwargs):
+        # form.Form plus uploadfieldclass instance
+        context = {}
+        files = self.request.FILES
+        form = UploadJSONandImage(self.request.POST, files)
+        if os.path.exists('upload_files/'):
+            shutil.rmtree('upload_files/')
+        if form.is_valid() and UploadJSONandImage.validate_extension(files.getlist('file_field')):
+            for file in files.getlist('file_field'):
+                upload_file_instance = UploadFileField(file_field=file)
+                upload_file_instance.save()
+                if UploadJSONandImage.get_extension(file.name) == 'json':
+                    json_file_url = upload_file_instance.file_field.url
+                    context['json_file_url'] = json_file_url
+                print('url?', upload_file_instance.file_field.url)
+                # upload_files/correlation_graph_y4r9KGs.json
+                print('path?', upload_file_instance.file_field.path)
+                #  /Users/mm17060/Projects/Research/sirius_graph_tool/upload_files/correlation_graph_y4r9KGs.json
+        else:
+            form_error = 'File Upload Error'
+            context['form_error'] = form_error
+        context['upload_form'] = form
+        return render(self.request, self.template_name, context)
+
+    def post_2(self, *args, **kwargs):
         """
         support upload multiple files in a POST request
         :return: a HTTPResponse
         """
         # files = self.request.FILES.getlist('file_field')
         files = self.request.FILES
-        print('what are files', self.request.FILES)
-        form = UploadJSONandImage(self.request.POST, files)
+        form = UploadJSONImageForm(self.request.POST, files)
         if form.is_valid():
-            for f in files.values():
-                print('what is the files', type(f))
-                # return a django.core.files.uploadedfile.InMemoryUploadedFile type
-
+            # only save one file here
+            form.save()
         context = {'upload_form': form}
         return render(self.request, self.template_name, context)
 
