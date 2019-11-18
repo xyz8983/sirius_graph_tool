4813a56206b151b037d9598793d77615cb47b200
Zheng, Yuhang <YZheng83@MassMutual.com>
2019-11-18 14:54:02 -0500

    upload and save multiple files

--- client/ts/index.ts
+++ client/ts/index.ts
@@ -2,5 +2,5 @@ import * as d3 from "d3";
 
 (function(){
    console.log('hey is it still alive');
-   d3.select("p").data(['hey','you']).enter().append('li').text(function(d){return "hello"+d;});
+   // d3.select("p").data(['hey','you']).enter().append('li').text(function(d){return "hello"+d;});
 }());
